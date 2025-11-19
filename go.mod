3bfb797d79c2f97779d16930923e85285631c0f6
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-11-19 14:34:47 -0300

    :sparkles: :whale: :wrench: feat: controllers, models, routes, docker-compose file and SQL file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-api-rest
 
 go 1.21.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- models/personalidades.go
+++ models/personalidades.go
@@ -1,6 +1,7 @@
 package models
 
 type Personalidade struct {
+	Id       int    `json:"id"`
 	Nome     string `josn:"nome"`
 	Historia string `json:"historia"`
 }
