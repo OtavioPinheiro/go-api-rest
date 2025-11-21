3009c2aa1b79f4d7f220a4c1c52a547744403bdb
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-11-21 17:33:43 -0300

    :sparkles: feat: Método POST para a rota de criação de personalidades

--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,7 @@ func HandleRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
-	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Gets")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriarUmaNovaPersonalidade).Methods("Post")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
