cb65aee374647438d1d6417a0b6825afae165c83
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-11-21 17:38:39 -0300

    :sparkles: feat: Criação da funcionalidade para deletar uma personalidade

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -40,3 +40,11 @@ func CriarUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
 	database.DB.Create(&novaPersonalidade)
 	json.NewEncoder(w).Encode(novaPersonalidade)
 }
+
+func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
