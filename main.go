8766ef9aa4d750bbdec3ede728d23c51a5d87f84
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-11-21 14:12:18 -0300

    :sparkles: feat: Criação da pasta database e do db.go -- Conexão com banco de dados via Go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go-api-rest/database"
 	"go-api-rest/models"
 	"go-api-rest/routes"
 )
@@ -11,7 +12,7 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
-
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando servidor REST com Go")
 	routes.HandleRequest()
 }
